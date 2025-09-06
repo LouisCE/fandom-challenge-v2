798ac7853f886c55f7be784111682654d3ab32d2
LouisCE <louiscowell190@yahoo.co.uk>
2025-09-06 02:02:46 +0100

    Add first Jak and Daxter sample question

---
+++ data.py
@@ -0,0 +1,15 @@
+JAK_QUESTIONS = [
+    {
+        "question": (
+            "In the first Jak and Daxter game, "
+            "what is the name of Jak's home village?"
+        ),
+        "options": [
+            "A) Rock Village",
+            "B) Sandover Village",
+            "C) Haven City",
+            "D) Volcanic Crater"
+        ],
+        "answer": "B"
+    }
+]
