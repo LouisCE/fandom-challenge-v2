3167fb6704d5984fb494f9e788cf2c6b16a3e0bc
LouisCE <louiscowell190@yahoo.co.uk>
2025-09-07 03:20:40 +0100

    Add Rules option to menu

--- run.py
+++ run.py
@@ -6,6 +6,15 @@ from data import JAK_QUESTIONS
 init(autoreset=True)
 print(Fore.GREEN + "Colorama test passed!")
 
+def rules():
+    """Display quiz rules to the player."""
+    print(Fore.MAGENTA + "\n=== QUIZ RULES ===")
+    print("1. You will be asked a series of questions.")
+    print("2. Each question has 4 options. Only one is correct.")
+    print("3. Type the number of your chosen answer and press Enter.")
+    print("4. Your score will be shown at the end of the quiz.")
+    print("5. Try to get the highest score you can!")
+    input(Fore.CYAN + "\nPress Enter to return to the menu...")
 
 def menu():
     while True:
@@ -16,7 +25,9 @@ def menu():
         print("4 - Exit")
         choice = input("Choose an option (1-4): ").strip()
 
-        if choice == "4":
+        if choice == "1":
+            rules()
+        elif choice == "4":
             print(Fore.YELLOW + "Goodbye!")
             sys.exit(0)
         else:
